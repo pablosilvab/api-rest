37eb24f8d567f854534e972c9b89f435fa7b1cdd
pablosilvab <pablonicolassilvabravo@gmail.com>
2017-05-05 00:07:44 -0300

    Login y registro de usuarios con Token

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@
 const express = require('express')
 const productCtrl = require('../controllers/product')
 const api = express.Router()
-
+const auth = require('../middlewares/auth')
 
 // Rutas
 api.get('/product', productCtrl.getProducts)
@@ -11,5 +11,9 @@ api.get('/product/:productId', productCtrl.getProduct)
 api.post('/product', productCtrl.saveProduct)
 api.put('/product/:productId', productCtrl.updateProduct)
 api.delete('/product/:productId', productCtrl.deleteProduct)
+
+api.get('/private', auth, ( req, res) => {
+  res.status(200).send({message: 'Tienes acceso.'})
+})
 
 module.exports = api
